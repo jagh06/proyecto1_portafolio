e5d7d5a279cb29d0d0bd4145480e3ad167c0f708
Juan Antonio Guzmán Hernández <jhonguzmanhernandez659@gmail.com>
2023-05-29 19:15:43 -0600

    se cambio el nombre

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Freelancer - Start Bootstrap Theme</title>
+        <title>Portafolio/title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Font Awesome icons (free version)-->
@@ -42,7 +42,7 @@
                 <!-- <img class="masthead-avatar mb-5" src="assets/img/avataaars.svg" alt="..." /> -->
                 <img class="masthead-avatar mb-5" src="assets/img/user.png" alt="..." />
                 <!-- Masthead Heading-->
-                <h1 class="masthead-heading text-uppercase mb-0">Juan Guzmán</h1>
+                <h1 class="masthead-heading text-uppercase mb-0">Juan Guzmán hernandez</h1>
                 <!-- Icon Divider-->
                 <div class="divider-custom divider-light">
                     <div class="divider-custom-line"></div>
@@ -74,6 +74,31 @@
         </section>
         <!-- About Section-->
         <section class="page-section bg-primary text-white mb-0" id="about">
+            <div class="container">
+                <!-- About Section Heading-->
+                <h2 class="page-section-heading text-center text-uppercase text-white">¿Quien soy?</h2>
+                <!-- Icon Divider-->
+                <div class="divider-custom divider-light">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+                <!-- About Section Content-->
+                <div class="row">
+                    <div class="col-lg-4 ms-auto"><p class="lead">Soy estudiante de Ingenieria en Desarrollo  y Gestiónde Software en la Universidad Tecnológica de la Selva. Me especializo en desarrollar aplicaiones web y aplicaciones móviles multiplataforma.</p></div>
+                    <div class="col-lg-4 me-auto"><p class="lead">Hé aprendido a manejar Laravel con PHP, Javascript, Java, Python, Flutter y React </p></div>
+                </div>
+                <!-- About Section Butto -->
+                <div class="text-center mt-4">
+                    <a class="btn btn-xl btn-outline-light" href="https://startbootstrap.com/theme/freelancer/">
+                        <i class="fas fa-download me-2"></i>
+                        Visitar canal de YouTube
+                    </a>
+                </div>
+            </div>
+        </section>
+         <!-- About Section-->
+         <section class="page-section bg-primary text-white mb-0" id="about">
             <div class="container">
                 <!-- About Section Heading-->
                 <h2 class="page-section-heading text-center text-uppercase text-white">¿Quien soy?</h2>
