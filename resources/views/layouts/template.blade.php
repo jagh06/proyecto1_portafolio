1fcf0140a26e4b5918a7e78e035c017667144937
Juan Antonio Guzmán Hernández <jhonguzmanhernandez659@gmail.com>
2023-06-05 21:19:30 -0600

    templatre modificado

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -117,7 +117,7 @@
                     <a class="btn btn-xl btn-outline-light" href="https://startbootstrap.com/theme/freelancer/">
                         <i class="fas fa-download me-2"></i>
                          Mis Trabajos en GitHub
-                         Migos
+                         Migos migos
                     </a>
                 </div>
             </div>
